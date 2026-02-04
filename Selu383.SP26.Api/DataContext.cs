72d1c09181918838b5282f24a041521f200c01e8
John Montz <john.montz@selu.edu>
2026-02-03 23:02:39 -0600

    FirstCommit
    -endpoints fixed

---
+++ Selu383.SP26.Api/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace Selu383.SP26.Api
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+		}
+		public DbSet<Location> Locations { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<Location>().HasData(
+	new Location { Id = 1, Name = "First Seed", Address = "Address 1", TableCount = 15 }
+);
+
+
+		}
+	}
+}
+
